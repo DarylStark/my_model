8029dfde34e2d2056f2d38ee71aaa5a78d7d7a9c
Daryl Stark <daryl@dstark.nl>
2023-05-05 23:44:23 +0200

    Created the `User` model

---
+++ src/my_model/user.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from enum import Enum
+
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+from pydantic import BaseModel, Field
+from pyotp import TOTP, random_base32
+
+
+class UserRole(int, Enum):
+    """ Enum containing the roles a user can have. """
+
+    root = 1
+    admin = 2
+    user = 3
+
+
+class User(BaseModel):
+    """ The user model is meant for local useraccounts """
+
+    created: datetime = Field(default_factory=datetime.utcnow)
+    fullname: str = Field(regex=r'^[A-Za-z\- ]+$', max_length=128)
+    username: str = Field(regex=r'^[a-zA-Z][a-zA-Z0-9_.]+$', max_length=128)
+    email: str = Field(
+        regex=r'^[a-z0-9_\-\.]+\@[a-z0-9_\-\.]+', max_length=128)
+    role: UserRole = Field(default=UserRole.user)
+    password_hash: str
+    password_date: datetime = Field(default_factory=datetime.utcnow)
+    second_factor: None | str = Field(
+        default=None,
+        regex=r'[A-Z0-9]+', max_length=64)
+
+    def set_password(self, password: str) -> bool:
+        """ Sets the password for the user """
+        hasher = PasswordHasher()
+        self.password_hash = hasher.hash(password)
+        self.password_date = datetime.utcnow()
+
+    def set_random_second_factor(self) -> str:
+        """ Sets a random second factor secret for the user and
+            returns the set secret """
+        self.second_factor = random_base32()
+        return self.second_factor
+
+    def disable_second_factor(self) -> None:
+        """ Disables the second factor for the user """
+        self.second_factor = None
+
+    def verify_credentials(self, username: str, password: str, second_factor: str | None = None) -> bool:
+        """ Verifies the credentials for a user """
+
+        hasher = PasswordHasher()
+        try:
+            credentials = (username == self.username and
+                           hasher.verify(self.password_hash, password))
+        except VerifyMismatchError:
+            return False
+
+        if second_factor is None:
+            return credentials
+        return credentials and second_factor == TOTP(self.second_factor).now()
---
+++ tests/test_users.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+
+from pyotp import TOTP
+from pytest import fixture, raises
+
+from my_model.user import User
+
+
+@fixture
+def example_user_no_second_factor() -> User:
+    user = User(
+        created=datetime.utcnow(),
+        fullname='Fake fullname',
+        username='fake.user',
+        email='fakse@fake.example',
+        password_hash='xxxxx',
+        password_date=datetime.utcnow(),
+        second_factor=None)
+    user.set_password('testtest')
+    return user
+
+
+def test_users_wrong_username():
+    """ Unit test to check if usernames that are invalid fail """
+
+    # List with wrong usernames
+    wrong_usernames = [
+        '', 'user name', 'user+name',
+        'user@domain.example', '1username', '_username',
+        '-username'
+    ]
+
+    # Loop through them and make sure they fail
+    for wrong_username in wrong_usernames:
+        with raises(ValueError):
+            User(
+                created=datetime.now(),
+                fullname='Fake fullname',
+                username=wrong_username,
+                email='fakse@fake.example',
+                password_hash='xxxxx',
+                password_date=datetime.now(),
+                second_factor=None)
+
+
+def test_users_correct_credentials(example_user_no_second_factor: User):
+    """ Test if credentials can be set and verified """
+
+    # Test username/password combination
+    assert example_user_no_second_factor.verify_credentials(
+        username='fake.user',
+        password='testtest')
+
+    # Create a OTP for the user
+    otp_secret = example_user_no_second_factor.set_random_second_factor()
+
+    # Test username/password/second factor combination
+    assert example_user_no_second_factor.verify_credentials(
+        username='fake.user',
+        password='testtest',
+        second_factor=TOTP(otp_secret).now())
+
+
+def test_users_incorrect_credentials(example_user_no_second_factor: User):
+    """ Test if wrong credentials result in error """
+
+    # Test username/password combination
+    assert not example_user_no_second_factor.verify_credentials(
+        username='fake.user',
+        password='somethingelse')
+
+    # Create a OTP for the user
+    _ = example_user_no_second_factor.set_random_second_factor()
+
+    # Test username/password/second factor combination
+    assert not example_user_no_second_factor.verify_credentials(
+        username='fake.user',
+        password='testtest',
+        second_factor='alsowrong')
