25b45685922739e1bf44208c187e1cbeda4a8222
Daryl Stark <daryl@dstark.nl>
2023-05-09 20:54:35 +0200

    Added a model for Web UI settings

---
+++ src/my_model/web_ui_setting.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+
+
+class WebUISetting(BaseModel):
+    """ Model for Web UI settings """
+
+    setting: str = Field(max_length=32)
+    value: str = Field(max_length=32)
