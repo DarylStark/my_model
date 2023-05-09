b02bd39310604777f45fc07da85c3fcdc4a416af
Daryl Stark <daryl@dstark.nl>
2023-05-09 21:17:34 +0200

    Updated test names

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -12,7 +12,7 @@ def example_api_client() -> APIClient:
     return client
 
 
-def test_api_token_random_token(example_api_client: APIClient) -> None:
+def test_api_client_random_token(example_api_client: APIClient) -> None:
     """ Unit test to see if we can set a random token """
 
     # Set a random token for the API token
@@ -20,7 +20,7 @@ def test_api_token_random_token(example_api_client: APIClient) -> None:
     assert token == example_api_client.token
 
 
-def test_api_token_overwrite(example_api_client: APIClient) -> None:
+def test_api_client_overwrite(example_api_client: APIClient) -> None:
     """ Test if we can overwrite the token with Force """
 
     token = example_api_client.set_random_token()
@@ -28,7 +28,7 @@ def test_api_token_overwrite(example_api_client: APIClient) -> None:
     assert token != new_token
 
 
-def test_api_token_not_overwrite(example_api_client: APIClient) -> None:
+def test_api_client_not_overwrite(example_api_client: APIClient) -> None:
     """ Test if we get an error when we try to overwrite the token
         after it is already set """
 
